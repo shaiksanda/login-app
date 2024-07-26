f5bbdd2df364b355219eca54ade033456f893b42
Shaik Sandani Basha <shaiksandanibasha7@gmail.com>
2024-07-26 06:25:11 +0000

    Initial commit

---
+++ src/components/Logout/index.js
@@ -0,0 +1,14 @@
+import './index.css'
+
+const Logout = props => {
+  const {logout} = props
+
+  return (
+    <button type="button" className="logout-button" onClick={logout}>
+      Logout
+    </button>
+  )
+}
+
+export default Logout
+// Write your code here
